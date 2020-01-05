9ab16e61eda906068d05e9e18773d069266b5ff7
felipecespedes <felipecespedespisso@gmail.com>
2020-01-05 00:53:15 -0500

    adds scrollview, removes unused dependencies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,18 +3,20 @@ import { HomeContainer } from './Home.styles';
 import Header from '../Header/Header';
 import Banking from '../Banking/Banking';
 import Market from '../Market/Market';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, ScrollView } from 'react-native';
 
 const Home = (props) => {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
       <SafeAreaView>
-        <HomeContainer>
-          <Header navigation={props.navigation} />
-          <Banking />
-          <Market />
-        </HomeContainer>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <HomeContainer>
+            <Header navigation={props.navigation} />
+            <Banking />
+            <Market />
+          </HomeContainer>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
